690ad82974a7b84d1227ac946b83d932c72cfaac
vaylonfernandes@gmail.com <vaylon-fernandes>
2022-07-01 22:31:22 +0530

    Update database

--- website/__init__.py
+++ website/__init__.py
@@ -9,20 +9,18 @@ DB_NAME = "tasks.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import ToDoList, User
-
-    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -30,12 +28,10 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        print(f"user {user_id}")
         return User.query.get(int(user_id))
+
     return app
 
 
 def create_database(app):
-    if not path.exists("website/"+DB_NAME):
-        db.create_all(app=app)
-        print("Database Created")
+    db.create_all(app=app)
